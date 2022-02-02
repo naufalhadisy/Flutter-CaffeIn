f953156e85028aebf58d962b2d8e6d550895d264
kivuro <hadinaufal26@gmail.com>
2022-02-02 22:02:04 +0700

    add customer booking history

--- lib/screens/customer_screens/history/booking_history.dart
+++ lib/screens/customer_screens/history/booking_history.dart
@@ -1,5 +1,7 @@
 import 'package:caffein_teamzeal/components/size_config.dart';
+import 'package:caffein_teamzeal/models/booking_model.dart';
 import 'package:caffein_teamzeal/screens/customer_screens/history/components/body.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookingHistory extends StatelessWidget {
@@ -8,12 +10,39 @@ class BookingHistory extends StatelessWidget {
   const BookingHistory({Key? key, required this.passes}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference booking = firestore.collection('booking');
     SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Booking History"),
       ),
-      body: Body(),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          StreamBuilder<QuerySnapshot>(
+              stream: booking.orderBy('Book_ID', descending: false).snapshots(),
+              builder: (_, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: (snapshot.data! as QuerySnapshot)
+                        .docs
+                        .map(
+                          (e) => Body(BookingModel(
+                              Book_id: e['Book_ID'],
+                              tabletype: e['tableType'],
+                              date: e['date'],
+                              numberperson: e['numberperson'],
+                              time: e['time'],
+                              additional: e['additional'])),
+                        )
+                        .toList(),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              }),
+        ]),
+      ),
     );
   }
 }
--- lib/screens/customer_screens/history/components/body.dart
+++ lib/screens/customer_screens/history/components/body.dart
@@ -1,27 +1,91 @@
 import 'package:caffein_teamzeal/components/loading.dart';
 import 'package:caffein_teamzeal/components/rich_text_row.dart';
 import 'package:caffein_teamzeal/components/size_config.dart';
+import 'package:caffein_teamzeal/models/booking_model.dart';
+import 'package:caffein_teamzeal/screens/customer_screens/booking/booking_form.dart';
 import 'package:caffein_teamzeal/screens/customer_screens/history/booking_history_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class Body extends StatelessWidget {
+  final BookingModel booking;
+  Body(this.booking);
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<BookingHistoryViewModel>.reactive(
       builder: (context, viewModel, child) {
         return SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.only(
+            left: 10.0,
+            right: 10.0,
+          ),
           child: SizedBox(
             width: double.infinity,
             child: SingleChildScrollView(
               child: Column(
                 children: [
                   SizedBox(height: getProportionateScreenHeight(5)),
+                  SizedBox(
+                    height: 15,
+                    width: 10,
+                  ),
+                  Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xFFCFB476))),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                            width: 10,
+                          ),
+                          Text(
+                            '   ${booking.tabletype}',
+                            //textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 15,
+                                // fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                          SizedBox(height: 10, width: 1),
+                          Text('   Date :          ${booking.date}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  // fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                          SizedBox(height: 3, width: 1),
+                          Text('   Person(s) :   ${booking.numberperson}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  // fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                          SizedBox(height: 3, width: 1),
+                          Text('   Time :          ${booking.time}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  // fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                          SizedBox(height: 3, width: 1),
+                          Text('   Note :          ${booking.additional}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  // fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                          const SizedBox(height: 15)
+                        ],
+                      )),
+                  const SizedBox(height: 30)
                 ],
               ),
             ),
           ),
-        );
+        ));
       },
       viewModelBuilder: () => BookingHistoryViewModel(),
     );
