645634590423391eede0593682fb564129a4ac10
kivuro <hadinaufal26@gmail.com>
2022-02-03 14:30:00 +0700

    Finalizing (add credit)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
+// MAP - PROJECT
+// TEAM ZEAL
+
+// MEMBERS:
+// AMMAR HAWARI ABDILLAH        (A19EC0220)
+// NAUFAL HADI SYUKRIMA         (A19EC0272)
+// SHAFIA CARINA SAPTOMO        (A19EC0286)
+// RAMADHAN MUHAMMAD JUMANTORO  (A19EC0277)
+
 import 'package:caffein_teamzeal/app.locator.dart';
 import 'package:caffein_teamzeal/app.router.dart';
 import 'package:caffein_teamzeal/theme.dart';
